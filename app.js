fc7b46755b9be05f91bc5e1163d1c1cb0a579802
MjrKong42 <mjr.kong42@gmail.com>
2021-03-22 10:20:32 -0500

    DOM Events Exercise w Bonus

--- app.js
+++ app.js
@@ -25,14 +25,12 @@ const form = document.querySelector(`form`);
 
 // 3b. Using addEventListener, display the value in the text input with an alert when the submit input in the form is pressed.
 
-const quote = `Z`
+form.addEventListener(`submit`, z => {
+    z.preventDefault();
+    const quote = form.elements.quote.value;
 
-form.addEventListener(`submit`, () => {
-    // p.preventDefault();
-    quote = userForm.elements.quote.value;
-    // const username = userForm.elements.username.value;
-    //   alert(quote);
-    alert(quote);
+      alert(quote);
+ 
 });
 
 // 4a. Select the Dark Mode button and set it to a const variable called darkMode
@@ -41,10 +39,38 @@ const darkMode = document.querySelector(`#dm`);
 
 // 4b. Using addEventListener, TOGGLE the "dark-mode" CSS class On/Off for EVERY element when the Dark Mode Button is clicked (HINT: You will need to use the asterisk "*" selector as well as a loop)
 
-
+darkMode.addEventListener(`click`, () => {
+    for (element of document.querySelectorAll(`*`)){
+      element.classList.toggle(`dark-mode`);
+    }
+  });
+  
 
 // 5a. Select the "MOVE TO ANOTHER REALITY" button and set it to a const variable called reality
 
-
+const reality = document.querySelector(`#reality`);
 
 // 5b. Using addEventListener, have an alert that says "You have successfully moved to another reality" pop up when you click on the button. However, have this setup so that when the button is clicked a third time have the following alert display instead of the "You have successfully moved to another reality" alert. New alert "OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!". And finally, when the button is clicked a fourth, fifth, sixth, etc. time have it do nothing (AKA Disable the button).
+
+let i = 1;
+function realityJump(){
+  if (i < 3){
+    alert("You have successfully moved to another reality");
+    i++;
+  } else {
+    alert("OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!");
+    reality.removeEventListener(`click`, realityJump);
+  }
+}
+reality.addEventListener(`click`, realityJump);
+// Or
+
+// let i = 1;
+// reality.addEventListener(`click`, () => {
+//   if (i < 3){
+//     alert("You have successfully moved to another reality");
+//   } else if (i === 3) {
+//     alert("OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!");
+//   }
+//   i++;
+// });
